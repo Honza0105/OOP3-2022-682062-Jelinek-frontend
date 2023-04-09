1aba955e6c4fb4395037af04f8fb433de5cd67f1
Honza0105 <honzajelinek@email.cz>
2023-04-09 14:30:51 +0200

    front end kinda done

---
+++ src/main/java/testingFrontend/accesingFile.java
@@ -0,0 +1,13 @@
+package testingFrontend;
+
+import testing.OtherModuleTest;
+
+public class accesingFile {
+    public static void main(String[] args) {
+
+    }
+
+    public void test(){
+        OtherModuleTest.main(null);
+    }
+}
